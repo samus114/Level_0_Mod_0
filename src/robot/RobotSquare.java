1bd35f857615297cba4a2cd37704037ead48b5a2
samus114 <kempiak@gmail.com>
2017-08-08 11:56:52 -0700

    Add files via upload

--- src/robot/RobotSquare.java
+++ src/robot/RobotSquare.java
@@ -6,22 +6,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
+Robot dog=new Robot("");
 
-fgart
         // 3. Put the robot's pen down
-
-
-        // 6. Make the robot move as fast as possible
-
+dog.penDown();
+       
+// 6. Make the robot move as fast as possible
+dog.setSpeed(10);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i <4; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+dog.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+dog.turn(90);
 
+    }}
 
-    }
+	private static Robot newRobot(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
